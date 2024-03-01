4f43eb1b5e24d6054dbfcaf659988c72b1072801
neiltejada <Neiltejada86@gmail.com>
2024-02-29 19:50:10 -0800

    thursday class

---
+++ content/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=225)
+    descrtiption = models.TextField()
+    year = models.IntegerField()
+    image = models.ImageField(upload_to='static/img/')
+    repository = models.URLField()
+    skill = models.ManyToManyField(Skill)
+
+    def __str__(self):
+        return f"{self.name} - ({self.year})"
